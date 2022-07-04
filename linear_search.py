fb710cde663bd8fa57f99a3636963748574b9d7d
koomekevo <koome.kevo@gurucrafts.net>
2022-07-04 17:43:32 +0300

    Write correct code for linear_search.py

--- linear_search.py
+++ linear_search.py
@@ -1,12 +1,20 @@
 def linear_search(list, target):
     for i in range(0, len(list)):
         if list[i] == target:
-            return "Target is :" [i]
-        else:
-            return "Target not found"
+            return i
+    return None
+
 
 def verify(index):
-    numbers = [1,2,3,4,5,6,7,8,9]
-    result = linear_search(numbers, 10)
+    if index is not None:
+        print("Target found at index: ", index)
+    else:
+        print("Target not found in the list")
 
-    
+numbers = [1,2,3,4,5,6,7,8,9]
+
+result = linear_search(numbers, 6)
+verify(result)
+
+result = linear_search(numbers, 7)
+verify(result)
