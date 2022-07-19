776b84b76c7825914be5545904d419947b065cdb
koomekevo <koome.kevo@gurucrafts.net>
2022-07-19 17:14:19 +0300

    Add left_half and right_half

--- merge_sort.py
+++ merge_sort.py
@@ -10,3 +10,7 @@ def merge_sort(list):
     
     if len(list) <= 1:
         return list
+    
+    left_half, right_half = split(list)
+    left = merge_sort(left_half)
+    right = merge_sort(right_half)
