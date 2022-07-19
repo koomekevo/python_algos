248536e3d2393fa5960c5323325334b95380242c
koomekevo <koome.kevo@gurucrafts.net>
2022-07-19 17:48:24 +0300

    Add alist

--- merge_sort.py
+++ merge_sort.py
@@ -46,3 +46,17 @@ def merge(left, right):
         else:
             l.append(right[j])
             j += 1
+    
+    while i < len(left):
+        l.append(left[i])
+        i += 1
+    
+    while j < len(right):
+        l.append(right[j])
+        j += 1
+
+    return l
+
+alist = [54, 62, 93, 17, 77, 31, 44, 55, 20]
+l = merge_sort(alist)
+print(l)
